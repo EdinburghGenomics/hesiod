cea17db7bfda357067da9d853d1f6f8f35215512
Tim Booth <tim.booth@ed.ac.uk>
2019-07-04 16:19:26 +0100

    Build in support for multiple instruments from the start.
    Our promethion appears to be named EGS1

--- list_remote_cells.sh
+++ list_remote_cells.sh
@@ -9,17 +9,20 @@ set -euo pipefail
 
 pattern='*/*/20??????_*_????????/fast?_????'
 
-if [[ -z "$UPSTREAM" ]] ; then
+if [[ -z "$UPSTREAM_LOC" ]] ; then
     # Nothing to do
     exit 0
-elif [[ "$UPSTREAM" =~ : ]] ; then
+elif [[ "$UPSTREAM_LOC" =~ : ]] ; then
     # This works as long as there are no rogue spaces.
-    ls_cmd="ssh ${UPSTREAM%%:*} cd ${UPSTREAM#*:} && ls -d $pattern"
+    ls_cmd="ssh ${UPSTREAM_LOC%%:*} cd ${UPSTREAM_LOC#*:} && ls -d $pattern"
     # Prevent glob expansion in local shell
     set -o noglob
 else
-    ls_cmd="cd ${UPSTREAM} && ls -d $pattern"
+    ls_cmd="cd ${UPSTREAM_LOC} && ls -d $pattern"
 fi
+
+# UPSTREAM_NAME must be set
+instrument="$UPSTREAM_NAME"
 
 # Chop the last dir name and condense all the results.
 # Then plonk the date from the first flowcell onto the run name.
@@ -31,13 +34,18 @@ while read l ; do
 
     this_dir="${l%%/*}"
     if [[ "$this_dir" != "$last_dir" ]] ; then
+        # Get the date from the last dir, which the machine generates.
         cell_dir="${l##*/}"
         cell_date="${cell_dir%%_*}"
-        if [[ "$this_dir" =~ ^${cell_date}_ ]] ; then
-            last_munged="$this_dir"
-        else
-            last_munged="${cell_date}_${this_dir}"
-        fi
+
+        # Peel off the date and instrument from the name, if present, then
+        # add them back.
+        last_munged="${this_dir}"
+        last_munged="${last_munged#${cell_date}_}"
+        last_munged="${last_munged#${instrument}_}"
+        last_munged="${cell_date}_${instrument}_${last_munged}"
+
+        # Remember this name for all lines with this run name
         last_dir="$this_dir"
     fi
     echo "$last_munged"$'\t'"$l"
