115c1d48dd0aaf0100531d942b93fea1d54a87b7
Tim Booth <tim.booth@ed.ac.uk>
2019-07-29 11:31:08 +0100

    I think we are ready for a test deployment

--- make_report.py
+++ make_report.py
@@ -11,7 +11,7 @@ import base64
 
 from hesiod import hesiod_version, glob, parse_cell_name, load_yaml
 
-def format_report(all_info, pipedata, run_status, aborted_list):
+def format_report(all_info, pipedata, run_status, aborted_list=(), minionqc=None):
     """Makes the report as a list of strings (lines)
     """
 
@@ -30,62 +30,114 @@ def format_report(all_info, pipedata, run_status, aborted_list):
 
     # Run metadata
     P()
-    P( "# About this run (experiment? project?)")
-    P()
+    P( "# About this run (experiment? project?)\n")
+    P( "### Metadata\n")
     P( '<dl class="dl-horizontal">' )
     P( '<dt>RunID</dt> <dd>{}</dd>'.format(",".join(runs)) )
     P( '<dt>Instrument</dt> <dd>{}</dd>'.format(",".join(instr)) )
     P( '<dt>CellCount</dt> <dd>{}</dd>'.format(len(all_info)) )
     P( '<dt>LibraryCount</dt> <dd>{}</dd>'.format(len(libs)) )
     P( '<dt>StartTime</dt> <dd>{}</dd>'.format((pipedata['start_times'] or ['unknown'])[0]) )
-    P( '<dt>LastCellTime</dt> <dd>{}</dd>'.format((pipedata['start_times'] or ['unknown'])[-1]) )
+    P( '<dt>LastRunTime</dt> <dd>{}</dd>'.format((pipedata['start_times'] or ['unknown'])[-1]) )
     P( '</dl>' )
 
-    for cell, ci in all_info.items():
+    # Overview plots from minionqc/combinedQC
+    if minionqc:
+        P('\n### {}\n'.format("MinionQC: Combined Length Histo ; Combined Quality Histo ; Combined Yield over Time"))
+        P("<div class='flex'>")
+        P(" ".join(
+            "[plot](img/minqc_combined_{f}.png){{.thumbnail}}".format(f=f)
+            for f in ['combined_length_histogram', 'combined_q_histogram', 'yield_over_time']
+         ))
+        P("</div>")
+
+    for cell, ci in sorted(all_info.items()):
         P()
-        P( "## Cell {}".format(cell) )
+        P( "# Cell {}".format(cell) )
         P()
         P( ":::::: {.bs-callout}" )
-        P( '<dl class="dl-horizontal">' )
 
         # Basic cell metadata
+        P( '<dl class="dl-horizontal">' )
+        P("### Metadata")
         for k, v in ci.items():
             if not k.startswith("_"):
+                # Special case for date
+                if k == "Date" and re.match(r'[0-9]{8}', v):
+                    v = datetime.strptime(v, '%Y%m%d').strftime('%d %b %Y')
                 P('<dt>{}</dt> <dd>{}</dd>'.format(k,escape(v)))
         P( '</dl>' )
         P()
 
+        # Nanoplot stats
+        if '_nanoplot' in ci:
+            ns = load_yaml(ci['_nanoplot'])
+
+            ''' # Selective version??
+            P("### Nanoplot General summary")
+            nsgs, = [ i[1] for i in ns if i[0] == "General summary" ]
+            for k, pv, n*_ in nsgs:
+                P('<dt>{}</dt> <dd>{}</dd>'.format(k,escape(pv)))
+            '''
+
+            for title, items in ns:
+                P( '<dl class="dl-horizontal">' )
+                P("### Nanoplot {}\n".format(escape(title)))
+                for k, pv, *_ in items:
+                    P('<dt>{}</dt> <dd>{}</dd>'.format(escape(k),escape(pv)))
+                P( '</dl>' )
+                P()
+
         # Embed some files from MinionQC
         minqc_base = os.path.dirname(ci['_minionqc'])
-        for f in ['length_histogram.png', 'length_vs_q.png', 'yield_over_time.png']:
+        '''
+        for f in ['length_histogram', 'length_vs_q', 'yield_over_time']:
             res.append('\n### {}\n'.format(f))
             res.append("<div class='flex'>")
             res.append(" ".join(
-                    "[plot](img/minqc_{ci[Library]}_{ci[CellID]}_{f}){{.thumbnail}}".format(ci=ci, f=f)
+                    "[plot](img/minqc_{ci[Library]}_{ci[CellID]}_{f}.png){{.thumbnail}}".format(ci=ci, f=f)
                     for _ in [1]
                 ))
             res.append("</div>")
+        '''
+        # Or more compactly...
+        P('\n### {}\n'.format("MinionQC: Length Histo ; Length vs Qual ; Yield over Time"))
+        P("<div class='flex'>")
+        P(" ".join(
+            "[plot](img/minqc_{ci[Library]}_{ci[CellID]}_{f}.png){{.thumbnail}}".format(ci=ci, f=f)
+            for f in ['length_histogram', 'length_vs_q', 'yield_over_time']
+         ))
+        P("</div>")
+
+        # Nanoplot plots
+        P('\n### {}\n'.format("NanoPlot: Length Histo ; Length vs Qual ; Yield over Time"))
+        P("<div class='flex'>")
+        P(" ".join(
+            "[plot](img/nanoplot_{ci[Library]}_{ci[CellID]}_{f}.png){{.thumbnail}}".format(ci=ci, f=f)
+            for f in ['HistogramReadlength', 'LengthvsQualityScatterPlot_dot', 'NumberOfReads_Over_Time']
+         ))
+        P("</div>")
 
 
-        # Link to the NanoPlot report (for now - should embed thumbnails)
-        P( "[NanoPlot Report](NanoPlot_{ci[Library]}_{ci[CellID]}-report.html)".format(ci=ci) )
+        # Link to the NanoPlot report
+        P( "[Full NanoPlot Report](NanoPlot_{ci[Library]}_{ci[CellID]}-report.html)".format(ci=ci) )
 
         # Blob plots as per SMRTino (the YAML file is linked rather than embedded but it's the
         # same otherwise)
         if '_blobs' in ci:
             for plot_group in load_yaml(ci['_blobs']):
 
-                res.append('\n### {}\n'.format(plot_group['title']))
+                P('\n### {}\n'.format(plot_group['title']))
 
                 # plot_group['files'] will be a a list of lists, so plot
                 # each list a s a row.
                 for plot_row in plot_group['files']:
-                    res.append("<div class='flex'>")
-                    res.append(" ".join(
-                            "[plot](img/{f}){{.thumbnail}}".format(f=os.path.basename(p))
-                            for p in plot_row
-                        ))
-                    res.append("</div>")
+                    P("<div class='flex'>")
+                    P(" ".join(
+                        "[plot](img/{f}){{.thumbnail}}".format(f=os.path.basename(p))
+                        for p in plot_row
+                     ))
+                    P("</div>")
 
         P( "::::::" )
 
@@ -119,7 +171,8 @@ def main(args):
     rep = format_report(all_info,
                         pipedata = pipedata,
                         run_status = status_info,
-                        aborted_list = status_info.get('CellsAborted'))
+                        aborted_list = status_info.get('CellsAborted'),
+                        minionqc = args.minionqc)
 
     if (not args.out) or (args.out == '-'):
         print(*rep, sep="\n")
@@ -131,9 +184,9 @@ def main(args):
         copy_dest = os.path.dirname(args.out) or '.'
         L.info("Copying files to {}".format(copy_dest))
 
-        copy_files(all_info, copy_dest)
+        copy_files(all_info, copy_dest, minionqc=args.minionqc)
 
-def copy_files(all_info, base_path):
+def copy_files(all_info, base_path, minionqc=None):
     """ We need to copy the NanoPlot, MinionQC, Blob reports into here.
         Base path will normally be wherever the report is being made.
     """
@@ -155,12 +208,23 @@ def copy_files(all_info, base_path):
             dest_rep = "NanoPlot_{ci[Library]}_{ci[CellID]}-report.html".format(ci=ci)
             shutil.copy(src_rep, os.path.join(base_path, dest_rep))
 
+            for png in glob(nano_base + '/*.png'):
+                dest_png = "nanoplot_{ci[Library]}_{ci[CellID]}_{f}".format(ci=ci, f=os.path.basename(png))
+                shutil.copy(png, os.path.join(base_path, "img", dest_png))
+
         if '_minionqc' in ci:
             min_base = os.path.dirname(ci['_minionqc'])
 
             for png in glob(min_base + '/*.png'):
                 dest_png = "minqc_{ci[Library]}_{ci[CellID]}_{f}".format(ci=ci, f=os.path.basename(png))
                 shutil.copy(png, os.path.join(base_path, "img", dest_png))
+
+    # Combined plots for MinionQC are separate
+    if minionqc:
+        cmin_base = os.path.dirname(minionqc)
+        for png in glob(cmin_base + '/*.png'):
+            dest_png = "minqc_combined_{f}".format(f=os.path.basename(png))
+            shutil.copy(png, os.path.join(base_path, "img", dest_png))
 
 def get_pipeline_metadata(pipe_dir):
     """ Read the files in the pipeline directory to find out some stuff about the
@@ -188,7 +252,7 @@ def get_pipeline_metadata(pipe_dir):
                  start_times = [ l.split('@')[1] for l in starts ],
                  rundir = rundir )
 
-def escape(in_txt, backwhack=re.compile(r'([][\`*_{}()#+-.!])')):
+def escape(in_txt, backwhack=re.compile(r'([][\`*_{}()#+-.!<>])')):
     """ HTML escaping is not the same as markdown escaping
     """
     return re.sub(backwhack, r'\\\1', str(in_txt))
@@ -220,7 +284,7 @@ def parse_args(*args):
                             help="Supply a list of info.yml files to compile into a report.")
     argparser.add_argument("--minionqc", default=None,
                            help="Add minionqc combined stats")
-    argparser.add_argument("-p", "--pipeline", default="rundir/pipeline",
+    argparser.add_argument("-p", "--pipeline", default="rundata/pipeline",
                             help="Directory to scan for pipeline meta-data.")
     argparser.add_argument("-s", "--status", default=None,
                             help="File containing status info on this run.")
