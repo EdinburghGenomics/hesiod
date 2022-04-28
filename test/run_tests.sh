caffb63b60ca0a4c1f7a7bb0b3449d20d8fa3493
Tim Booth <tim.booth@ed.ac.uk>
2022-04-28 12:36:30 +0100

    Upgrade a bunch of pip packages. Tweak the run_tests.sh script to use the _hesiod_venv by default.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-set -euo pipefail
 
 # When running the tests, we need to ensure Python picks up the right environment.
 # For this reason it's worth having a test wrapper.
+cd "`dirname $0`"/.. || exit 1
 
 # Most tests currently pass using the system Python3 but really you should test with
-# the VEnv Python3. I guess this script could activate it for you...
+# the VEnv Python3. Let's activate this for you now, before we 'set -eu'.
+if [ -n "$VIRTUAL_ENV" ] ; then
+    echo "Virtual Env already active: $VIRTUAL_ENV"
+elif [ -e _hesiod_venv ] ; then
+    echo "Running: source ./_hesiod_venv/bin/activate"
+    source ./_hesiod_venv/bin/activate
+    if [ "$(which python3)" != "$(readlink -f _hesiod_venv)/bin/python3" ] ; then
+        echo "FAILED - python3 is $(which python3) not $(readlink -f _hesiod_venv)/bin/python3"
+        exit 1
+    fi
+else
+    echo "No ./_hesiod_venv; will proceeed using the default $(which python3)"
+fi
 
-cd "`dirname $0`"/..
+# Now we can...
+set -euo pipefail
 
 export RUN_SLOW_TESTS=${RUN_SLOW_TESTS:-0}
 export RUN_NETWORK_TESTS=${RUN_NETWORK_TESTS:-1}
